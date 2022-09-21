637ad58aea53946c738b4b77987b2d4407b43162
luciaenriquezbos <eboscreative@gmail.com>
2022-09-21 16:41:27 +0200

    Chore: angular concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Lucia';
+  age = 18;
+  img = "https://i.blogs.es/bbe976/12260-simone-cmoon/1366_2000.webp";
+  btnDisabled = true;
+  person = {
+    name: "Lucia",
+    age: 18,
+    avatar: "https://i.blogs.es/bbe976/12260-simone-cmoon/1366_2000.webp"
+  }
+  names: string[] = ['nico', 'juli', 'santi'];
+  newName = '';
+  products = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+
+  changeName (event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+    }
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number){
+    this.names.splice(index, 1);
+  }
+
 }
